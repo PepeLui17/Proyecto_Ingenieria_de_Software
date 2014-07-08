5998b0ffda77a3c1524c0ee380b0755d22bc82c2
Jose Luis <jl_mj_17@hotmail.com>
2014-07-07 22:39:27 -0500

    Actualización del proyecto
    Se ha añadido la configuración de Spring para autorización de usuarios,
    además se han modificado las vistas, algunas de ellas copiadas en la
    carpeta administrador.

---
+++ LibreriaDonQuijote/src/main/java/com/managedBeans/loginBean.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.managedBeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author José Luis
+ */
+@ManagedBean
+@SessionScoped
+public class loginBean {
+
+    private String admin;
+    private String vendedor;
+      
+    
+    public boolean esAdministrador(String usuario)
+    {
+        if (usuario.equals("admin")) 
+        {
+            return true;
+        } 
+        else 
+        {
+            return false;
+        }
+    }
+    
+    public boolean esVendedor(String usuario){
+        if (usuario.equals("jlmj")) {
+            return true;
+        } else {
+        return false;}
+    }
+}
